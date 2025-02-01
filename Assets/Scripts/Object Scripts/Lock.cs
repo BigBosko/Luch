897015f2e76d4b8adb2e0b9a50b004f6954508de
Boštjan Skubic <bskubic07@gmail.com>
2025-02-01 21:18:30 +0100

    Dodelanje lockkers
    Omarice se po uspešnem odklenjenju odprejo

--- Assets/Scripts/Object Scripts/Lock.cs
+++ Assets/Scripts/Object Scripts/Lock.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Lock : Interactable
 {
+
     public int lockId;
+    public bool isLocked = true;
 
-
+     
     public override void Interact()
     {
         Debug.Log("Interacting with Lock ID: " + lockId);
         InventoryHandler inventory = FindObjectOfType<InventoryHandler>();
         if (inventory.HasRightKey(lockId))
         {
+            isLocked = false;
+            Transform door = transform.parent;
+
+            door.Rotate(0, -90, 0);
             Debug.Log("Lock unlocked!");
         }
         else
--- Assets/Scripts/Player Scripts/PlayerInteraction.cs
+++ Assets/Scripts/Player Scripts/PlayerInteraction.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     public DetectionHandler detectionHandler;
-    public InventoryHandler inventory;
 
     void Update()
     {
