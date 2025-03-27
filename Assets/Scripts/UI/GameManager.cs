9c193806ad4fc74b9263a4eff5a42db17daeabe2
Boštjan Skubic <bskubic07@gmail.com>
2025-03-27 12:00:12 +0100

    Pred izbrisom skupnega navmesha
    Dodani navmesh posebej na vse gameobjecte

--- Assets/Scripts/UI/GameManager.cs
+++ Assets/Scripts/UI/GameManager.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEditor.AI;
+using Unity.AI.Navigation;
 public class GameManager : MonoBehaviour
 {
-
+    [SerializeField] private NavMeshSurface navMeshSurface;
 
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("Main Menu");
     }
 
+    public void RebuildNavMesh()
+    {
+        navMeshSurface.BuildNavMesh();
+    }
 }
+
